5bf9fc2b7c4d7c28e8df7f566cb6b33ba5a058fb
Marc Khoury <marc.khoury@isae.edu.lb>
2019-04-14 06:10:53 -0700

    Add files via upload

---
+++ IHMFahrenheit.java
@@ -0,0 +1,46 @@
+package question3;
+import question1.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class IHMFahrenheit extends JFrame implements ActionListener{
+  private JTextField entree = new JTextField( 6 );
+  /** Le bouton de conversion. */
+  private JButton  boutonDeConversion = new JButton( "convertir" );
+  /** La sortie en degré Celsius. */
+  private JTextField sortie = new JTextField( 6 );
+
+  public IHMFahrenheit(){
+    super("IHM Fahrenheit");
+ 
+    setLayout(new FlowLayout());
+    add( entree ); add( boutonDeConversion ); add( sortie );
+    sortie.setEditable( false );
+    getContentPane().setBackground( Color.pink );
+    setLocation(100,100);
+    pack();setVisible(true);
+    
+    boutonDeConversion.addActionListener( this );
+  }
+
+ 
+  public void actionPerformed( ActionEvent ae ){
+    try{
+      int fahrenheit = 0;
+      float celsius = 0F; 
+      fahrenheit= Integer.parseInt(entree.getText());
+      celsius=FahrenheitCelsius.fahrenheitEnCelsius(fahrenheit);
+      if(celsius < -273.1F) 
+      celsius = -273.1F;
+      sortie.setText(Float.toString(celsius));
+    }catch(NumberFormatException nfe){
+      sortie.setText("error ! ");
+    }
+  }
+  
+  
+  public static void main(String[] args){
+      new IHMFahrenheit();
+    }
+}
