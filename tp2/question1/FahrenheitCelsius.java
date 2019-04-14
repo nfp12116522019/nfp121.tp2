ad82e33a8da0e29f78f3684c53cb5fc20e8dee41
Marc Khoury <marc.khoury@isae.edu.lb>
2019-04-14 06:09:15 -0700

    Add files via upload

--- tp2/question1/FahrenheitCelsius.java
+++ tp2/question1/FahrenheitCelsius.java
@@ -1,41 +1,25 @@
 package question1;
-
-/**
- * Décrivez votre classe FahrenheitCelsius ici.
- * 
- * @author (votre nom)
- * @version (un numéro de version ou une date)
- */
 public class FahrenheitCelsius {
 
-	/**
-	 * le point d'entrée de cette application, dont le commentaire est à
-	 * compléter
-	 * 
-	 * @param args
-	 *            ...
-	 */
 	public static void main(String[] args) {
-		// pour tous les paramètres de la ligne de commande
+	   
 		int fahrenheit = 0;
 		float celsius = 0;
+		
+		for(int x=0;x<args.length;x++){
+		    fahrenheit=Integer.parseInt(args[x]);
+		    celsius=fahrenheitEnCelsius(Integer.parseInt(args[x]));
 		System.out.println(fahrenheit + "\u00B0F -> " + celsius + "\u00B0C"); // ligne,
 																				// format
 																				// imposés
 																				
-	}
+	
+}}
 
-	/**
-	 * la méthode à compléter.
-	 * 
-	 * @param f
-	 *            la valeur en degré Fahrenheit
-	 * @return la conversion en degré Celsius
-	 */
 	public static float fahrenheitEnCelsius(int f) {
-
-		return 0.F; // à compléter en remplaçant ce return 0.F par la fonction
-					// de conversion
+                float celsius=5/9f * (f-32);
+                 float resultat=(int)(celsius*10)/10.0f;
+		return resultat;
 	}
 
 }
